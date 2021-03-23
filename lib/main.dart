5813845d2b1b1824c8f5d5954d557883f96ee8ec
takumi <53643382+takumi1005@users.noreply.github.com>
2021-03-23 22:13:20 +0900

    ようつべっぽいの

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,136 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_app/video_detail_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final items = List<String>.generate(10000, (i) => "Item $i");
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('YouTubeアプリ'),
+          centerTitle: false,
+          leading: Icon(Icons.videocam),
+          title: const Text(
+            'YouTubeアプリ',
+          ),
+          actions: <Widget>[
+            SizedBox(
+              width: 44,
+              child: FlatButton(
+                child: Icon(Icons.search),
+                onPressed: () {
+                  // todo
+                },
+              ),
+            ),
+            SizedBox(
+              width: 44,
+              child: FlatButton(
+                child: Icon(Icons.more_vert),
+                onPressed: () {
+                  // todo
+                },
+              ),
+            ),
+          ],
         ),
-        body: Center(
-          child: Container(),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 60,
+                      height: 60,
+                      child: Image.network(
+                        'https://www.pakutaso.com/shared/img/thumb/kemuriIMGL7004_TP_V.jpg',
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        const Text(
+                          'sampleでーす',
+                        ),
+                        FlatButton(
+                          child: Row(
+                            children: <Widget>[
+                              Icon(
+                                Icons.video_call,
+                                color: Colors.red,
+                              ),
+                              Text('登録する'),
+                            ],
+                          ),
+                          onPressed: () {
+                            // todo
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      onTap: () async {
+                        await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => VideoDetailPage()));
+                      },
+                      contentPadding: EdgeInsets.all(8),
+                      leading: Image.network(
+                        'https://www.pakutaso.com/shared/img/thumb/kemuriIMGL7004_TP_V.jpg',
+                      ),
+                      title: Column(
+                        children: <Widget>[
+                          Text(
+                            '【Flutter超入門】リストを作る方法',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Text(
+                                '300回再生',
+                                style: TextStyle(
+                                  fontSize: 13,
+                                ),
+                              ),
+                              Text(
+                                '5日前',
+                                style: TextStyle(
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      trailing: Icon(Icons.more_vert),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
